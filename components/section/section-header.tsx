97d5375d5e2c7e52bf77d27d26025eb6a4cb20c7
andrwyoung <yong.andrew11@gmail.com>
2025-05-30 14:19:06 -0700

    added back in block adder

--- components/section/section-header.tsx
+++ components/section/section-header.tsx
@@ -3,20 +3,26 @@
 
 import { Section } from "@/types/board-types";
 import InlineEditText from "../ui/inline-edit";
-import {
-  updateSectionDescription,
-  updateSectionTitle,
-} from "@/lib/sync/section-actions";
+import { updateSectionTitle } from "@/lib/sync/section-actions";
 import { useLoadingStore } from "@/store/loading-store";
-import { useUIStore } from "@/store/ui-store";
+import { FaPlus } from "react-icons/fa";
+import { useImagePicker } from "@/hooks/use-image-picker";
+import { createTextBlock } from "@/lib/sync/text-block-actions";
 
 export default function SectionHeader({ section }: { section: Section }) {
   const title = section?.title;
-  const description = section?.description;
-
-  const mirrorMode = useUIStore((s) => s.mirrorMode);
   const isEditing =
     useLoadingStore.getState().editingSectionId === section.section_id;
+
+  const { triggerImagePicker, fileInput } = useImagePicker(section.section_id);
+
+  function handleAddImageBlock() {
+    triggerImagePicker();
+  }
+
+  function handleAddTextBlock() {
+    createTextBlock(section.section_id);
+  }
 
   return (
     <div className="flex flex-row justify-between items-center pt-6 pb-0 px-3">
@@ -32,19 +38,51 @@ export default function SectionHeader({ section }: { section: Section }) {
           className="text-lg sm:text-xl md:text-2xl text-left"
         />
       </div>
-      {!mirrorMode && (
-        <div className="hidden lg:flex w-xs">
-          <InlineEditText
-            value={description && description.trim() != "" ? description : null}
-            unnamedPlaceholder="Click to add a Description"
-            placeholder="Add a Description"
-            onChange={(newDesc) => {
-              updateSectionDescription(section.section_id, newDesc);
-            }}
-            className="text-sm text-right"
-          />
+      <div className="flex flex-row gap-2 items-center">
+        {/* {!mirrorMode && (
+          <div className="hidden lg:flex w-xs">
+            <InlineEditText
+              value={
+                description && description.trim() != "" ? description : null
+              }
+              unnamedPlaceholder="Click to add a Description"
+              placeholder="Add a Description"
+              onChange={(newDesc) => {
+                updateSectionDescription(section.section_id, newDesc);
+              }}
+              className="text-sm text-right"
+            />
+          </div>
+        )} */}
+        {/* <FaPlus className="text-primary hover:text-accent cursor-pointer" /> */}
+        <div className="group flex flex-row cursor-pointer text-primary">
+          {fileInput}
+          <div
+            className="hidden group-hover:block font-header px-1 font-semibold hover:text-accent transition-all duration-300"
+            onClick={() => handleAddTextBlock()}
+          >
+            Text
+          </div>
+          <div
+            className="flex-shrink-0 relative size-6 group cursor-pointer hover:scale-95 
+            transition-transform duration-200 flex items-center justify-center"
+          >
+            {/* <div className="absolute inset-0 rounded-full border-4 border-primary" /> */}
+            <FaPlus className="z-2 size-4 text-primary group-hover:text-accent hover:primary transition-colors duration-300" />
+            {/* <div
+              className="absolute inset-0 rounded-full bg-primary/40 z-1 group-hover:bg-background transition-all duration-300
+              group-hover:scale-30"
+            /> */}
+          </div>
+
+          <div
+            className="hidden group-hover:block font-header px-1 font-semibold hover:text-accent transition-all duration-300"
+            onClick={() => handleAddImageBlock()}
+          >
+            Image
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
