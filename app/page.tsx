4ca23adb825f33978a086d368da059d884c5fef0
andrwyoung <yong.andrew11@gmail.com>
2025-08-05 16:10:56 -0700

    copy

--- app/page.tsx
+++ app/page.tsx
@@ -145,7 +145,7 @@ export default function Home() {
               </h1>
               <div className="flex flex-col items-center">
                 <p className="max-w-lg text-xs sm:text-sm text-accent font-bold">
-                  Early Testing • Come check it out!
+                  Early Testing • Come Check it Out!
                 </p>
                 {/* <p className="text-lg mb-14 w-full font-semibold">
                   For illustrators who’d rather draw than organize reference
